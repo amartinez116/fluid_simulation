0c675406cacd41103c9a098f470b65336016def6
amartinez116 <amartin1@andrew.cmu.edu>
2021-04-26 19:48:54 -0400

    adding build dir

---
+++ SPH/build/CMakeFiles/SPH.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPH.dir/fluid.cpp.o"
+  "CMakeFiles/SPH.dir/main.cpp.o"
+  "CMakeFiles/SPH.dir/particle.cpp.o"
+  "SPH"
+  "SPH.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
